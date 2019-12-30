fbeca6b9e45aaaa6548f5e037dd1e905a967026b
coder <ngochieu10041997@gmail.com>
2019-12-30 11:26:36 +0700

    Fix conflict

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 # Enable build
 export DOCKER_CLI_EXPERIMENTAL=enabled
-<<<<<<< HEAD
-=======
-
->>>>>>> BUILDX setup
 # Fix binfmt_misc
 docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
